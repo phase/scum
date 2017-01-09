81cbab1d0455d97c9e788d35cf7d48a4eb665016
Jadon Fowler <jadonflower@gmail.com>
2017-01-08 16:57:37 -0800

    Lots of bug fixes
    The game is getting to a useable state.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main/kotlin/xyz/jadonfowler/scum/Scum.kt
+++ src/main/kotlin/xyz/jadonfowler/scum/Scum.kt
@@ -20,6 +20,7 @@ fun main(args: Array<String>) {
 
 class Game(val players: MutableList<Player>) {
     var top: List<Card>? = null
+    val finishedPlayers = mutableListOf<Player>()
 
     fun handOutCards() {
         val deck = Deck()
@@ -30,7 +31,16 @@ class Game(val players: MutableList<Player>) {
         }
     }
 
+    fun reset() {
+        players.addAll(finishedPlayers)
+        finishedPlayers.clear()
+        top = null
+    }
+
     fun start() {
+        players.forEach {
+            it.sendMessage("Passing out cards...")
+        }
         handOutCards()
 
         val totalPlayers = players.size
@@ -40,11 +50,14 @@ class Game(val players: MutableList<Player>) {
             if (players.size <= i)
                 break
 
-            if (lastPlayed == i) {
+//            println("   $lastPlayed ${players.size} $i")
+            if (lastPlayed == i || (lastPlayed > (players.size - 1) && (i == players.size - 1))) {
                 top = null
-                players.forEach {
-                    it.sendMessage("Top card swiped.")
+                val swipeMessage = { p: Player ->
+                    p.sendMessage("Top card swiped.")
                 }
+                players.forEach(swipeMessage)
+                finishedPlayers.forEach(swipeMessage)
             }
 
             val player = players[i]
@@ -72,17 +85,19 @@ class Game(val players: MutableList<Player>) {
 
             val playerFinished = player.hand.size < 1
 
-            players.forEach {
+            val message = { p: Player ->
                 if (played == null)
-                    it.sendMessage("$player didn't play anything.")
+                    p.sendMessage("$player didn't play anything.")
                 else if (played!!.size == 1)
-                    it.sendMessage("$player played a ${played!![0]}.")
+                    p.sendMessage("$player played a ${played!![0]}.")
                 else
-                    it.sendMessage("$player played $played.")
+                    p.sendMessage("$player played $played.")
 
                 if (playerFinished)
-                    it.sendMessage("$player finished #${totalPlayers - players.size + 1}!")
+                    p.sendMessage("$player finished #${totalPlayers - players.size + 1}!")
             }
+            players.forEach(message)
+            finishedPlayers.forEach(message)
 
             // Swipe if 2
             if (played?.get(0)?.kind == Kind.TWO) {
@@ -95,18 +110,36 @@ class Game(val players: MutableList<Player>) {
             }
 
             // If the card is the same, skip the next player
-            val skip = top?.value() == played?.value()
+            val skip = top != null && played != null && top?.value() == played.value()
 
             if (played != null && played[0].kind != Kind.TWO) {
                 top = played
                 lastPlayed = i
             }
-            if (player.hand.size < 1)
-                players.removeAt(i)
+            if (playerFinished) {
+                val p = players.removeAt(i)
+                finishedPlayers.add(p)
+            }
 
             if (++i > players.size - 1) i = 0
-            if (skip) if (++i > players.size - 1) i = 0
+            if (skip) {
+                if (players.size >= i) {
+                    val skippedMessage = { p: Player ->
+                        p.sendMessage("${players[i]} was skipped.")
+                    }
+                    players.forEach(skippedMessage)
+                    finishedPlayers.forEach(skippedMessage)
+                }
+
+                if (++i > players.size - 1) i = 0
+
+            }
         }
+        reset()
+        players.forEach {
+            it.sendMessage("The game has finished!")
+        }
+        start()
     }
 
 }
@@ -128,23 +161,45 @@ class ConsolePlayer(name: String) : Player(name) {
 
     override fun play(top: List<Card>?): List<Card>? {
         println()
-        if (top != null)
+
+        val validCards = hand.filter {
+            if (top != null) it.kind.int >= top.value()
+            else true
+        }
+
+        if (top != null) {
             println("The top card is: ${if (top.size > 1) top.toString()
             else top[0].toString()}.")
+
+            // If we can't play anything, don't play anything.
+            if (validCards.isEmpty()) {
+                println("You couldn't play anything.")
+                return null
+            }
+
+            // If we can only play our final card, play that card.
+            if (validCards.size == 1 && hand.size == 1) {
+                return listOf(validCards[0])
+            }
+        }
+
+        // If we can play any card and we only have one card, play that card.
+        if (top == null && hand.size == 1) return listOf(hand[0])
 
         println("Your hand:")
         hand.sort()
         hand.forEachIndexed { i, card ->
-            println("  [$i] $card")
+            // Put an X by the cards we can't use.
+            println("${if (validCards.contains(card)) " " else "X"} [${i + 1}] $card")
         }
 
         print("Please choose a card: ")
-        var input = readLine()
-        while (input == null) input = readLine()
+        val input = readLine()
+        if (input == null || input.isNullOrEmpty()) return null
 
         // Get one card
         if (input.matches(Regex("[0-9]+"))) {
-            val index = input.toInt()
+            val index = input.toInt() - 1
             if (index >= hand.size) return play(top)
             val card = hand[index]
             return listOf(card)
@@ -153,13 +208,19 @@ class ConsolePlayer(name: String) : Player(name) {
         else if (input.matches(Regex("[0-9]+, [0-9]+"))
                 || input.matches(Regex("[0-9]+, [0-9]+, [0-9]+"))
                 || input.matches(Regex("[0-9]+, [0-9]+, [0-9]+, [0-9]+"))) {
-            val indices = input.split(", ").map { it.toInt() }
+            val indices = input.split(", ").map { it.toInt() - 1 }
 
             // Check if there are multiple indices in the input
             val searchIndices = mutableListOf<Int>()
             indices.forEach {
                 if (searchIndices.contains(it)) return play(top)
                 else searchIndices.add(it)
+            }
+
+            // Make sure the indices are within the bounds of the hand
+            indices.forEach {
+                if (it < 0 || it > hand.size)
+                    return play(top)
             }
 
             val cards = indices.map { hand[it] }
@@ -176,11 +237,11 @@ class BotPlayer(i: Int) : Player("Bot $i") {
     }
 
     override fun play(top: List<Card>?): List<Card>? {
-
+        Thread.sleep(500) // Thinking takes a while
         if (top == null) return listOf(hand.last())
         val validCards = hand.filter { it.kind.int >= top.value() }.toMutableList().sorted().reversed()
+//        println("  Top Card: $top, $name's hand: $validCards from $hand.")
         if (validCards.isEmpty()) return null
-//        println("  $name's hand: $validCards.")
 
         val amountNeeded = top.size
         var amount = 0
@@ -188,9 +249,8 @@ class BotPlayer(i: Int) : Player("Bot $i") {
         var lastKind: Kind? = null
 
         validCards.forEachIndexed { i, card ->
-            if (amount == amountNeeded) {
+            if (amount == amountNeeded)
                 return indices.map { validCards[it] }
-            }
 
             if (lastKind == null) {
                 lastKind = card.kind
@@ -201,11 +261,15 @@ class BotPlayer(i: Int) : Player("Bot $i") {
                     indices.add(i)
                     amount++
                 } else {
-                    amount = 0
                     indices.clear()
+                    amount = 0
+                    lastKind = null
                 }
             }
         }
-        return null
+        if (amount == amountNeeded)
+            return indices.map { validCards[it] }
+        else
+            return null
     }
 }
